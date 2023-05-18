cbe3be0119277f907e8d71307cf1d48e6d03a1b6
susanhsrestha <susanshrestha783@gmail.com>
2023-05-18 14:23:09 +0545

    tweet scraping mechanism

--- scp_tweet.py
+++ scp_tweet.py
@@ -6,12 +6,25 @@ keywords = [
     "emergency",
     "natural disaster",
     "earthquake",
+    "flood",
+    "hurricane",
+    "wildfire",
+    "landslide",
+    "drought",
+    "pandemic",
+    "COVID-19",
+    "evacuation",
+    "rescue",
+    "casualties",
+    "death toll",
+    "hazard",
+    "crisis",
 ]
 # Construct the search query
 query = "(from:kathmandupost) " + " OR ".join(keywords)
 print(query)
-tweets = set()
-limit = 2000
+tweets = []
+limit = 50
 
 # Scrape tweets for each keyword in the list
 for keyword in keywords:
@@ -25,7 +38,7 @@ for keyword in keywords:
             continue
         else:
             # Append the current tweet and keyword to the list
-            tweets.add([tweet.date, tweet.username, tweet.content, keyword])
+            tweets.append([tweet.date, tweet.username, tweet.content, keyword])
 
 # Convert the list of tweets to a Pandas DataFrame and print it
 if not tweets:
@@ -33,4 +46,4 @@ if not tweets:
 else:
     df = pd.DataFrame(tweets, columns=['Date', 'User', 'Tweet', 'Keyword'])
     print(df)
-    df.to_csv('tw.csv')
+    df.to_csv('twt.csv')
